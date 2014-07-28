fc749fe345a846db88763ae97bcedf847138825b
LuiCat <zhtl1995@163.com>
2014-07-28 22:12:12 +0800

    Added d3d render test

--- src/danmakulyrica.cpp
+++ src/danmakulyrica.cpp
@@ -1,5 +1,7 @@
 #include "danmakulyrica.h"
 
+#include "graphics.h"
+
 DanmakuLyrica::DanmakuLyrica()
 {
 }
@@ -19,4 +21,26 @@ void DanmakuLyrica::mainUpdate()
 
 void DanmakuLyrica::mainRender()
 {
+    static int updateCount=0;
+
+    d3d.beginScene();
+
+    d3d.resetMatrix();
+    d3d.pushMatrix();
+
+    d3d.translate(400, 300);
+    d3d.scale(150.0, 150.0);
+    d3d.rotate(0.085*updateCount++);
+    d3d.translate(-0.5, -0.5);
+
+    d3d.pushVertex(0.0, 0.0, 0.0, 0.0);
+    d3d.pushVertex(1.0, 0.0, 1.0, 0.0);
+    d3d.pushVertex(1.0, 1.0, 1.0, 1.0);
+    d3d.pushVertex(0.0, 1.0, 0.0, 1.0);
+
+    d3d.popMatrix();
+
+    d3d.endScene();
+
+    d3d.present();
 }
