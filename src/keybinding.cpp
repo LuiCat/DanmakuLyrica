30ebe640a0c093ab340e68374ca227c12cdb352d
LuiCat <zhtl1995@163.com>
2014-08-11 21:50:54 +0800

    A modified version of key binding
    I guess I've got what mistake I have made....

--- src/keybinding.cpp
+++ src/keybinding.cpp
@@ -18,10 +18,9 @@ bool KeyBinding::useBufferedData = false;
 
 HRESULT DInput_Init(HWND hWnd, HINSTANCE hInst)
 {
-    HRESULT hr;
+    UNUSED(hInst);
 
-    if(FAILED(hr=DirectInput8Create(hInst, DIRECTINPUT_VERSION, IID_IDirectInput8A, (LPVOID*)&lpDirectInput, NULL)))
-        return hr;
+    HRESULT hr;
 
     if(FAILED(hr=KeyBinding::initDevice(hWnd)))
         return hr;
@@ -33,26 +32,26 @@ HRESULT DInput_Init(HWND hWnd, HINSTANCE hInst)
 void DInput_Cleanup()
 {
     KeyBinding::cleanupDevice();
-
-    if (lpDirectInput)
-    {
-        lpDirectInput->Release();
-        lpDirectInput=0;
-    }
 }
 
 
 KeyBinding::KeyBinding(DWORD _keyKeyboard, DWORD _keyJoystick)
+    :keyKeyboard(0)
+    ,keyJoystick(0)
+    ,pushed(false)
+    ,down(false)
+    ,value(0)
+    ,callbackFunc(0)
 {
     bindingList.push_back(this);
     setKey(_keyKeyboard, true);
-    setKey(_keyJoystick, true);
+    setKey(_keyJoystick, false);
 }
 
 KeyBinding::~KeyBinding()
 {
     setKey(0, true);
-    setKey(0, true);
+    setKey(0, false);
     bindingList.remove(this);
 }
 
@@ -140,7 +139,7 @@ bool KeyBinding::setKey(DWORD newKey, bool isKeyboard)
 
     if(newKey)
     {
-        map[(newKey)]=this;
+        map[newKey]=this;
     }
 
     oldKey=newKey;
@@ -155,111 +154,45 @@ void KeyBinding::setCallbackFunc(BindingCallback func)
 
 HRESULT KeyBinding::initKeyboard(HWND hWnd)
 {
-    HRESULT hr;
-
-    if(FAILED(hr=lpDirectInput->CreateDevice(GUID_SysKeyboard, &lpKeyboard, NULL)))
-        return hr;
-
-    if(FAILED(hr=lpKeyboard->SetDataFormat(&c_dfDIKeyboard)))
-        return hr;
-
-    if(FAILED(hr=lpKeyboard->SetCooperativeLevel(hWnd, DISCL_NONEXCLUSIVE | DISCL_FOREGROUND)))
-        return hr;
-
-    DIPROPDWORD     property;
-
-    property.diph.dwSize = sizeof(DIPROPDWORD);
-    property.diph.dwHeaderSize = sizeof(DIPROPHEADER);
-    property.diph.dwObj = 0;
-    property.diph.dwHow = DIPH_DEVICE;
-    property.dwData = DINPUT_BUFFERSIZE;
-
-    if(FAILED(hr=lpKeyboard->SetProperty(DIPROP_BUFFERSIZE, &property.diph)))
-        return hr;
-
+    UNUSED(hWnd);
     return S_OK;
 }
 
 void KeyBinding::cleanupKeyboard()
 {
-    if(lpKeyboard)
-    {
-        lpKeyboard->Unacquire();
-        lpKeyboard->Release();
-        lpKeyboard=0;
-    }
 }
 
 void KeyBinding::refreshKeyboard()
 {
-    if(!lpKeyboard)
-        return;
-
-    HRESULT hr = DI_OK;
-
-    BYTE                diks[256];
-    DIDEVICEOBJECTDATA  didod[DINPUT_BUFFERSIZE];
-    DWORD               dwElements;
-
     unordered_map<DWORD, KeyBinding*>::iterator iter;
 
     DWORD currentTick=GetTickCount();
 
-    hr=lpKeyboard->Acquire();
-
-    hr=DI_OK;
-
-    if(useBufferedData)
+    DWORD key;
+    for(auto x : bindingList)
     {
-        hr=lpKeyboard->GetDeviceData(sizeof(DIDEVICEOBJECTDATA), didod, &dwElements, 0);
-        if(hr==DI_OK || hr==DI_BUFFEROVERFLOW)
-        {
-            for(DWORD i=0; i<dwElements; ++i)
-            {
-                iter=keyboardMap.find(didod[i].dwOfs);
-                if(iter==keyboardMap.end())
-                    continue;
-                iter->second->pushKeyData(true, didod[i].dwOfs, didod[i].dwData, didod[i].dwTimeStamp);
-            }
-        }
+        key=x->keyKeyboard;
+        x->pushKeyData(true, key, GetAsyncKeyState(key), currentTick);
     }
 
-    if(!useBufferedData || hr==DI_BUFFEROVERFLOW)
+    for(auto x : bindingList)
     {
-        hr=lpKeyboard->GetDeviceState(256, diks);
-        if(hr==DI_OK)
-        {
-            DWORD key;
-            for(auto x : bindingList)
-            {
-                key=x->keyKeyboard;
-                if(diks[key] & 0x80)
-                    x->pushKeyData(true, key, diks[key], currentTick);
-            }
-        }
+        x->dealKeyData();
     }
 }
 
 HRESULT KeyBinding::initJoystick(HWND hWnd)
 {
     UNUSED(hWnd);
-    // ...
     return S_OK;
 }
 
 void KeyBinding::cleanupJoystick()
 {
-    if(lpJoystick)
-    {
-        lpJoystick->Unacquire();
-        lpJoystick->Release();
-        lpJoystick=0;
-    }
 }
 
 void KeyBinding::refreshJoystick()
 {
-    // ...
 }
 
 void KeyBinding::pushKeyData(bool isKeyboard, DWORD key, DWORD data, DWORD timeStamp)
@@ -271,6 +204,8 @@ void KeyBinding::pushKeyData(bool isKeyboard, DWORD key, DWORD data, DWORD timeS
 void KeyBinding::dealKeyData()
 {
     KeyData* keyData;
+
+    bool newDown;
 
     value=0;
     pushed=false;
@@ -284,12 +219,13 @@ void KeyBinding::dealKeyData()
 
         if(keyData->isKeyBoard)
         {
-            down=(((keyData->data)&0x80)!=0);
-            if(down)
+            newDown=(((SHORT)keyData->data)<0);
+            if(newDown && !down)
             {
                 pushed=true;
                 value++;
             }
+            down=newDown;
         }
         else
         {
