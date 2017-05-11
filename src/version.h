5e77ab8c1835220317a8bfee429b51ccdca3d5dd
LuiCat <zhtl1995@163.com>
2017-05-11 13:45:16 +0800

    added version header

---
+++ src/version.h
@@ -0,0 +1,8 @@
+﻿// 编码：Utf-8 with BOM
+// Created by LuiCat
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_RELEASE 1
+
+#define VERSION_STR "##VERSION_MAJOR##.##VERSION_MINOR##.##VERSION_RELEASE##"
